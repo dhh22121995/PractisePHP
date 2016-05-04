caeaf4115936f8a1e839b730b81c177e74d87d05
Doan Hoang Huy <dhh22121995@yahoo.com.vn>
2016-05-04 16:20:35 +0700

    update tryyourbest.php

--- tryYourBestSlogan.php
+++ tryYourBestSlogan.php
@@ -7,12 +7,23 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <title></title>
     </head>
     <body>
+
         <div class="fullscreenDiv">
-            <div class="center" style="font-size: 100px">Try your best!</div>
+
+            <div class="center" style="font-size: 100px">
+                <div class="container">
+                    <h2>Đoàn Hoàng Huy</h2>
+                    <img src="images/avatar.jpg" class="img-circle" alt="Cinque Terre" width="304" height="236"> 
+                </div>
+            </div>
         </div>
         <?php
         ?>
